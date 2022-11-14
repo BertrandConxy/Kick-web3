5093a19526845b46c82e7f4ba04cd6e2a1915ad7
Bertrand <mutanganabertrand@gmail.com>
2022-11-14 18:15:50 +0200

    Create campaign smart contract

--- contracts/campaign.sol
+++ contracts/campaign.sol
@@ -1,3 +1,44 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.7.0 <0.9.0;
+
+contract Campaign {
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+    }
+
+    address public manager;
+    uint public minimumContribution;
+    address[] public approvers;
+    Request[] public requests;
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    constructor(uint minimum) {
+        manager = msg.sender;
+        minimumContribution = minimum;
+    }
+    
+    function contribute() public payable {
+        require(msg.value > minimumContribution);
+        approvers.push(msg.sender);
+    }
+
+    function createRequest(string memory _description, uint _value, address _recipient)
+        public view restricted {
+            Request memory newRequest = Request({
+                description: _description,
+                value: _value,
+                recipient: _recipient,
+                complete: false
+            });
+
+        }
+
+}
