264e740161bec68a0112666fd2eaaa1e6eca3c0c
Bertrand <mutanganabertrand@gmail.com>
2022-12-13 09:48:13 +0200

    Add layout

--- pages/index.js
+++ pages/index.js
@@ -2,46 +2,45 @@ import React from 'react'
 import instance from '../contracts/factory.js'
 import 'semantic-ui-css/semantic.min.css'
 import { Card, Button, Icon, Grid, Container } from 'semantic-ui-react'
-import Navigation from '../components/Navigation.js'
+import Layout from '../components/Layout.js'
 
 const Home = ({ deployedCampaigns }) => {
   return (
-    <>
-      <Navigation />
-      <div style={styles.container}>
-        <Container>
-          <Grid columns={2}>
-            <Grid.Row>
-              <Grid.Column>
-                <h3>Open Campaigns</h3>
-              </Grid.Column>
-              <Grid.Column>
-                <Button
-                  floated="right"
-                  content="Create Campaign"
-                  icon="add circle"
-                  primary
-                />
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-          <Card.Group>
-            {deployedCampaigns.map((address) => {
-              return (
-                <Card key={address}>
-                  <Card.Content>
-                    <Card.Header style={styles.wrap}>{address}</Card.Header>
-                  </Card.Content>
-                  <Card.Content extra>
-                    <Button primary>View Campaign</Button>
-                  </Card.Content>
-                </Card>
-              )
-            })}
-          </Card.Group>
-        </Container>
-      </div>
-    </>
+      <Layout>
+        <div style={styles.container}>
+          <Container>
+            <Grid columns={2}>
+              <Grid.Row>
+                <Grid.Column>
+                  <h3>Open Campaigns</h3>
+                </Grid.Column>
+                <Grid.Column>
+                  <Button
+                    floated="right"
+                    content="Create Campaign"
+                    icon="add circle"
+                    primary
+                  />
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+            <Card.Group>
+              {deployedCampaigns.map((address) => {
+                return (
+                  <Card key={address}>
+                    <Card.Content>
+                      <Card.Header style={styles.wrap}>{address}</Card.Header>
+                    </Card.Content>
+                    <Card.Content extra>
+                      <Button primary>View Campaign</Button>
+                    </Card.Content>
+                  </Card>
+                )
+              })}
+            </Card.Group>
+          </Container>
+        </div>
+      </Layout>
   )
 }
 
